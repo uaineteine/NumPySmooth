6fa4dc9d661a62dc8809afaa259d67fdae120608
Daniel Stamer-Squair <drsta3@student.monash.edu>
2019-03-28 13:18:03 -0700

    smoothing functions all done and ready for deployment (hopefully no bugs). Mega happy about this

--- NpSmooth.py
+++ NpSmooth.py
@@ -15,9 +15,30 @@ def meanSmooth(time, nparray, nsmooth):#get odd (eg. 3, 5 smoothing factor)
         t.append(time[i])
     return np.array(t), np.array(out)
 
+def medianSmooth(time, nparray, nsmooth):#get odd eg (3, 5 smth factor)
+    out = []
+    n = int((nsmooth + 1)/ 2)
+    t = []
+    for i, val in enumerate(nparray):
+        if i > 0 and i < len(nparray) -2:
+            values = []
+            for k in range(-n + 1, n): #in 3 smooting goes from -1 to 1
+                ind = i + k 
+                values.append(nparray[ind])
+                
+            out.append(np.median(values))
+            t.append(time[i])
+
 def meanSmthIt(time, nparray, nsmooth, iterations):
     smtht = time
     smth = nparray
     for i in range(0, iterations):
         smtht, smth = meanSmooth(smtht, smth, nsmooth)
+    return smtht, smth
+
+def medianSmthIt(time, nparray, nsmooth, iterations):
+    smtht = time
+    smth = nparray
+    for i in range(0, iterations):
+        smtht, smth = medianSmooth(smtht, smth, nsmooth)
     return smtht, smth
